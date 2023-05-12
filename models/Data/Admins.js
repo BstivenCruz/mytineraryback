ffc76a7ea9873c53ec427e88fe132a9e537c8587
stiven_cruz <bseguracruz@yahoo.com>
2023-05-12 11:07:31 -0500

    model y data

---
+++ models/Data/Admins.js
@@ -0,0 +1,54 @@
+[
+  {
+    id: "admin1",
+    name: "Pablo",
+    lastName: "Muñiz",
+    photo:
+      "https://img.freepik.com/fotos-premium/retrato-hombre-negocios-confiado-afeitar-que-ve-expresion-segura-si-mismo-listo-firmar-poses-contrato-cargo-fondo-blanco-usa-camisa-blanca-corbata-elegante-negocios-personas_95891-13837.jpg",
+    age: 27,
+    email: "pablo.muñiz@gmail.com",
+    password: "chau6789",
+    code: "equipo-rosa",
+    verified: true,
+    logged: true,
+  },
+  {
+    id: "admin2",
+    name: "Stiven",
+    lastName: "",
+    photo:
+      "https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+    age: 23,
+    email: "stiven.cruz@gmail.com",
+    password: "chau6789",
+    code: "equipo-rosa",
+    verified: true,
+    logged: true,
+  },
+  {
+    id: "admin3",
+    name: "Luciana",
+    lastName: "Rovere",
+    photo:
+      "https://i.pinimg.com/736x/15/1b/df/151bdfbc670e4c326bfe40be3fa2f242.jpg",
+    age: 26,
+    email: "luciana.rovere@gmail.com",
+    password: "chau6789",
+    code: "equipo-rosa",
+    verified: true,
+    logged: true,
+  },
+  {
+    id: "admin4",
+    name: "Juan",
+    lastName: "Gonzales",
+    photo:
+      "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+    age: 30,
+    email: "juan.gonzales@gmail.com",
+    password: "chau6789",
+    code: "equipo-rosa",
+    verified: true,
+    logged: true,
+  },
+];
