33a636aea4c2add634f14b0cf0316eeee4e96a78
stiven_cruz <bseguracruz@yahoo.com>
2023-05-20 15:23:31 -0500

    fix and routes

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,22 @@
-import  express from 'express';
+import express from "express";
+import passport from "passport";
+import user from "../controllers/users.js";
+const { signup, signin, signout , read} = user;
+
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("user");
 });
 
-export default router
+router.get("/", read);
+router.post("/signup", signup);
+router.post("/signin", signin);
+router.put(
+  "/signout",
+  passport.authenticate("jwt", { session: false }),
+  signout
+);
 
+export default router;
