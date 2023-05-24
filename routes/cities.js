53f5230de135a23b5a6fa613bb3695b7e07ead02
stiven_cruz <bseguracruz@yahoo.com>
2023-05-23 20:15:46 -0500

    read cities , users ready

--- routes/cities.js
+++ routes/cities.js
@@ -1,9 +1,9 @@
 import express from "express";
 import city from "../controllers/cities.js";
-const { read }=city
+const { read } = city;
 
 const router = express.Router();
 
 router.get("/", read);
 
-export default router
+export default router;
