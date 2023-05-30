34032ffbdd000576f32863611e4c3c422ad909a6
stiven_cruz <bseguracruz@yahoo.com>
2023-05-30 12:02:48 -0500

    schemas city

--- schemas/city.js
+++ schemas/city.js
@@ -3,7 +3,7 @@ import Joi from "joi-oid";
 const citySchema = Joi.object({
   name: Joi.string().min(1).max(50).required(),
   pais: Joi.string().min(1).max(50).required(),
-  photo: Joi.string().uri().min(1).max(50).required().messages({
+  photo: Joi.string().uri().min(1).max(200).required().messages({
     "any.required": "This field is empty! please complete",
   }),
   population: Joi.number().required(),
