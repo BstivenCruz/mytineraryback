d229902837c39f48079ab8a65b55fac34173bef3
stiven_cruz <bseguracruz@yahoo.com>
2023-06-02 11:42:41 -0500

    read places

---
+++ controllers/places.js
@@ -0,0 +1,31 @@
+import { Place } from "../models/Place.js";
+import defaultResponse from "../helpers/response.js";
+
+const place = {
+  read: async (req, res, next) => {
+    try {
+      const place = await Place.find().select(
+        " -_id  -createdAt -updatedAt -__v"
+      );
+      req.body.success = true;
+      req.body.sc = 201;
+      req.body.data = { place };
+      return defaultResponse(req, res);
+    } catch (error) {
+      next(error);
+    }
+  },
+  create: async (req, res, next) => {
+    try {
+      await Place.create(req.body);
+      req.body.success = true;
+      req.body.sc = 201;
+      req.body.data = "place created";
+      return defaultResponse(req, res);
+    } catch (error) {
+      next(error);
+    }
+  },
+};
+
+export default place;
